365f0488bc2c45dd9cbbb393a1ac62fbd1f41623
Banee Ishaque K <Baneeishaque@gmail.com>
2019-11-07 12:49:16 +0530

    In

---
+++ App_Code/DBhandler.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Data.SqlClient;
+
+public class DBhandler
+{
+    SqlConnection con;
+
+    public DBhandler()
+    {
+        ConnectionStringSettings settings = ConfigurationManager.ConnectionStrings["ConnectionString"];
+        if (settings != null)
+        {
+            con = new SqlConnection(settings.ConnectionString);
+        }
+    }
+
+    public string SingleInsert(String query)
+    {
+        if (con != null)
+        {
+            try
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand(query, con);
+                int result = cmd.ExecuteNonQuery();
+                con.Close();
+                if (result == 1)
+                {
+                    return "1";
+                }
+            }
+            catch (Exception exception)
+            {
+                return exception.Message;
+            }
+        }
+        return "0";
+    }
+}
+
+
+
+
+
---
+++ App_Code/Service.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.Services;
+
+[WebService(Namespace = "http://tempuri.org/")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+public class Service : System.Web.Services.WebService
+{
+    public Service()
+    {
+    }
+
+    [WebMethod]
+    public string InsertJob(string equipment, string jobNumber, string itemNumber, string coreNumber, string coreTotal, string phase, string status)
+    {
+        DBhandler db = new DBhandler();
+        return db.SingleInsert("INSERT INTO equipment_scrum(equipment, job, item, core, core_total, phase, status) VALUES('" + equipment + "', " + jobNumber + ", " + itemNumber + ", " + coreNumber + ", " + coreTotal + ", '" + phase + "', '" + status + "')");
+    }
+}
